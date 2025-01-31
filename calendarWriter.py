d80a9027a5bc5ff597da026ecd24f73b20f2524e
Dustin Brothers <gentian.limited-0t@icloud.com>
2025-01-31 12:16:59 -0700

    Updates to the calendar writer. It is now producing correctly structured CalDav events given test info from one of the N2YO JSON files stored by the dataFetcher.py

--- calendarWriter.py
+++ calendarWriter.py
@@ -1,9 +1,18 @@
 
+################################################################################
+#                               Description
+################################################################################
+# The purpose of this script is to read in a local .json file which contains 
+# information about a satellite's sky traversions and convert each event into
+# a single CalDav file with a unique UUID as a filename.
+
 #############################################
 # Structure of a CalDav File
 #############################################
+
 # The RFC of the specification can be found here:
 # https://www.rfc-editor.org/rfc/rfc4791
+#
 ################################
 # Boilerplate
 ################################
@@ -67,11 +76,6 @@
 #           UID:E1CD3F83-3AB9-46CC-8048-508A4E65D01B
 #           X-WR-ALARMUID:E1CD3F83-3AB9-46CC-8048-508A4E65D01B
 #       END:VALARM
-#
-#       BEGIN:VALARM
-#           ACTION:NONE
-#           TRIGGER;VALUE=DATE-TIME:19760401T005545Z
-#       END:VALARM
 #   END:VEVENT
 # END:VCALENDAR
 
@@ -92,69 +96,196 @@ def genRandomId():
 ################################################################################
 import datetime, dateutil
 
-def utcEpochToLocalTime(thisEpoch: int, myTimezone: str):
+def utcEpochToLocalTime(thisEpoch: int, myLocationString: str):
     # Convert the Epoch Time to a Timestamp object of the UTC timezone
     thisUtcTime = datetime.datetime.fromtimestamp(thisEpoch).replace(tzinfo=dateutil.tz.gettz('UTC'))
     # Convert the UTC Timestamp object to a Local Timestamp object
-    thisLocalTime = thisUtcTime.astimezone(dateutil.tz.gettz(myTimezone))
+    thisLocalTime = thisUtcTime.astimezone(dateutil.tz.gettz(myLocationString))
     # Format the timestamp to YYYYMMDDTHHMMSS
-    thisLocalTimeFormatted = thisLocal.strftime("%Y%m%dT%H%M%S")
+    thisLocalTimeFormatted = thisLocalTime.strftime("%Y%m%dT%H%M%S")
     # Return the local time
     return thisLocalTime
 
 def getCurrentLocalTime():
     return datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
 
+def getTimezoneInfo(thisTime: str, myLocationString: str):
+    # Generate the TZ Object for the location
+    thisTz = dateutil.tz.gettz(myLocationString)
+
+    # Get the TZ Offset and Name for this specific time
+    thisTzOffset = datetime.datetime.strptime(thisTime, "%Y%m%dT%H%M%S").replace(tzinfo=thisTz).strftime("%z")
+    thisTzName = datetime.datetime.strptime(thisTime, "%Y%m%dT%H%M%S").replace(tzinfo=thisTz).strftime("%Z")
+
+    # Return the values
+    return thisTzOffset, thisTzName
+
 ################################################################################
 #                             Calendar Event Building
 ################################################################################
-def generateAlert(minutesBefore: int):
-    newDataList = list()
+def generateVALARM(minutesBefore: int):
+    newAlarmList = list()
 
     # Of the form xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
     thisAlarmUID = genRandomId()
 
     # Append the alarm information
-    newDataList.append("BEGIN:VALARM")
-    newDataList.append("ACTION:DISPLAY")
-    newDataList.append("DESCRIPTION:Reminder")
-    newDataList.append("TRIGGER:-PT%iM"%(minutesBefore))
-    newDataList.append("UID:%s"%(thisAlarmUID))
-    newDataList.append("X-WR-ALARMUID:%s"%(thisAlarmUID))
-    newDataList.append("END:VALARM")
-    # newDataList.append("BEGIN:VALARM")
-    # newDataList.append("ACTION:NONE")
-    # This is the founding date of Apple Computer....probably not needed...
-    # newDataList.append("TRIGGER;VALUE=DATE-TIME:19760401T005545Z")
-    # newDataList.append("END:VALARM")
+    newAlarmList.append("    BEGIN:VALARM")
+    newAlarmList.append("      ACTION:DISPLAY")
+    newAlarmList.append("      DESCRIPTION:Reminder")
+    newAlarmList.append("      TRIGGER:-PT%iM"%(minutesBefore))
+    newAlarmList.append("      UID:%s"%(thisAlarmUID))
+    newAlarmList.append("      X-WR-ALARMUID:%s"%(thisAlarmUID))
+    newAlarmList.append("    END:VALARM")
 
     # Return the new list
-    return newDataList
+    return newAlarmList
 
-def generateEvent(eventSummary: str, startTime: str, endTime: str, myTimezone: str, 
-                  eventLocation: str, eventDescription: str, eventUrl: str, eventUid: str):
-    newDataList = list()
+def generateVEVENT(eventSummary: str, startTime: str, endTime: str, myLocationString: str, 
+                  eventLocation: str, eventDescription: str, eventUrl: str, eventUid: str,
+                   alarmMinutesBefore: int):
+    newEventList = list()
 
     # Convert the Start time to local in the correct format
-    localStartTime = utcEpochToLocalTime(thisEpoch=startTime, myTimezone=myTimezone)
+    localStartTime = utcEpochToLocalTime(thisEpoch=startTime, myLocationString=myLocationString)
     # Convert the End time to local in the correct format
-    localEndTime = utcEpochToLocalTime(thisEpoch=endTime, myTimezone=myTimezone)
+    localEndTime = utcEpochToLocalTime(thisEpoch=endTime, myLocationString=myLocationString)
     # Create the current timestamp
     thisCurrentTime = getCurrentLocalTime()
 
-    newDataList.append("BEGIN:VEVENT")
-    newDataList.append("SUMMARY:%s"%(eventSummary))
-    newDataList.append("DTSTART;TZID=%s:%s"%(myTimezone,localStartTime))
-    newDataList.append("DTEND;TZID=%s:%s"%(myTimezone,localEndTime))
-    newDataList.append("LOCATION:Location: %s"%(eventLocation))
-    newDataList.append("DESCRIPTION:%s"%(eventDescription))
-    newDataList.append("URL;VALUE=URI:%s"%(eventUrl))
-    newDataList.append("UID:%s"%(eventUid))
-    newDataList.append("TRANSP:OPAQUE")
-    newDataList.append("SEQUENCE:0")
-    newDataList.append("CREATED:%s"%(thisCurrentTime))
-    newDataList.append("LAST-MODIFIED:%s"%(thisCurrentTime))
-    newDataList.append("DTSTAMP:%s"%(thisCurrentTime))
+    # Create a list of the event information
+    newEventList.append("  BEGIN:VEVENT")
+    newEventList.append("    SUMMARY:%s"%(eventSummary))
+    newEventList.append("    DTSTART;TZID=%s:%s"%(myLocationString,localStartTime))
+    newEventList.append("    DTEND;TZID=%s:%s"%(myLocationString,localEndTime))
+    newEventList.append("    LOCATION:Location: %s"%(eventLocation))
+    newEventList.append("    DESCRIPTION:%s"%(eventDescription))
+    newEventList.append("    URL;VALUE=URI:%s"%(eventUrl))
+    newEventList.append("    UID:%s"%(eventUid))
+    newEventList.append("    TRANSP:OPAQUE")
+    newEventList.append("    SEQUENCE:0")
+    newEventList.append("    CREATED:%s"%(thisCurrentTime))
+    newEventList.append("    LAST-MODIFIED:%s"%(thisCurrentTime))
+    newEventList.append("    DTSTAMP:%s"%(thisCurrentTime))
+
+    # Generate and append an alert/reminder to the event
+    alarmList = generateVALARM(minutesBefore=alarmMinutesBefore)
+
+    # Add the alarm list to the new event list
+    newEventList.extend(alarmList)
+
+    # Close the event
+    newEventList.append("  END:VEVENT")
 
     # Return the new list
-    return newDataList
+    return newEventList
+
+def generateVTIMEZONE(myLocationString: str):
+    newTimezoneList = list()
+
+    # Get the Offset and Name
+    startOffset, startName = getTimezoneInfo(thisTime="20071104T020000", myLocationString=myLocationString)
+    endOffset, endName = getTimezoneInfo(thisTime="20070311T020000", myLocationString=myLocationString)
+
+    # This is static until Daylight savings changes in the US
+    newTimezoneList.append("  BEGIN:VTIMEZONE")
+    newTimezoneList.append("    TZID:%s"%(myLocationString))
+    newTimezoneList.append("    BEGIN:STANDARD")
+    newTimezoneList.append("      DTSTART:20071104T020000")
+    newTimezoneList.append("      RRULE:FREQ=YEARLY;BYMONTH=11;BYDAY=1SU")
+    newTimezoneList.append("      TZNAME:%s"%(startName))
+    newTimezoneList.append("      TZOFFSETFROM:%s"%(endOffset))
+    newTimezoneList.append("      TZOFFSETTO:%s"%(startOffset))
+    newTimezoneList.append("    END:STANDARD")
+    newTimezoneList.append("    BEGIN:DAYLIGHT")
+    newTimezoneList.append("      DTSTART:20070311T020000")
+    newTimezoneList.append("      RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU")
+    newTimezoneList.append("      TZNAME:%s"%(endName))
+    newTimezoneList.append("      TZOFFSETFROM:%s"%(startOffset))
+    newTimezoneList.append("      TZOFFSETTO:%s"%(endOffset))
+    newTimezoneList.append("    END:DAYLIGHT")
+    newTimezoneList.append("  END:VTIMEZONE")
+
+    # Return the new list
+    return newTimezoneList
+
+def generateCalDavEvent(eventSummary: str, startTime: str, endTime: str, myLocationString: str, 
+                  eventLocation: str, eventDescription: str, eventUrl: str, eventUid: str,
+                   alarmMinutesBefore: int):
+
+    newCalendarEntryList = list()
+
+    newCalendarEntryList.append("BEGIN:VCALENDAR")
+    newCalendarEntryList.append("  VERSION:2.0")
+    newCalendarEntryList.append("  CALSCALE:GREGORIAN")
+    newCalendarEntryList.append("  PRODID:-//Apple Inc.//macOS 13.7.1//EN")
+
+    # Generate the timezone fields
+    timezoneList = generateVTIMEZONE(myLocationString=myLocationString)
+
+    # Generate base event fields
+    eventList = generateVEVENT(
+        eventSummary=eventSummary,
+        startTime=startTime,
+        endTime=endTime,
+        myLocationString=myLocationString,
+        eventLocation=eventLocation,
+        eventDescription=eventDescription,
+        eventUrl=eventUrl,
+        eventUid=eventUid,
+        alarmMinutesBefore=alarmMinutesBefore
+    )
+
+    # Add the timezone list to the new calendar entry
+    newCalendarEntryList.extend(timezoneList)
+
+    # Add the base event list to the new calendar entry
+    newCalendarEntryList.extend(eventList)
+    
+    # Close the calendar entry
+    newCalendarEntryList.append("END:VCALENDAR")
+
+    # Return the full event
+    return newCalendarEntryList
+
+# Example string setup and call based on an N2YO JSON file:
+#
+# import calendarWriter
+#
+# # Test Data
+# satname = "NOAA 15"
+# startAz = 177.84
+# startAzCompass = "S"
+# startUTC = 1734660280
+# maxAz = 258.19
+# maxAzCompass = "W"
+# maxEl = 53.45
+# maxUTC = 1734660725
+# endAz = 341.99
+# endAzCompass = "NNW"
+# endUTC = 1734661175
+#
+# # Generate a random UID for this event:
+# thisEvenUid = calendarWriter.genRandomId()
+#
+# # Setup the Summary and Description strings:
+# eventSummary = "%s, Elevation: %.2f"%(satname, maxEl)
+# eventDescription = "Start: %s, Peak: %s, End: %s"%(startAzCompass, maxAzCompass, endAzCompass)
+#
+# # Setup the URL, just point to the N2YO page for now...
+# thisUrl = "www.n2yo.com"
+#
+# newEvent = calendarWriter.generateCalDavEvent(
+#   eventSummary=eventSummary,
+#   startTime=startUTC,
+#   endTime=endUTC,
+#   myLocationString='America/Denver',
+#   eventLocation='Outer Space',
+#   eventDescription=eventDescription,
+#   eventUrl=thisUrl,
+#   eventUid=thisEvenUid,
+#   alarmMinutesBefore=15
+# )
+#
+# for thisLine in newEvent:
+#   print(thisLine)
